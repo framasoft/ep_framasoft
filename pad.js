cd9878891e669e576300e11bfb9327402230046b
JosephK <josephk@framasoft.org>
2016-07-20 08:38:49 +0200

    Update pad.js

--- pad.js
+++ pad.js
@@ -1,5 +1,5 @@
 var eejs = require('ep_etherpad-lite/node/eejs/');
 // Add client code
 exports.eejsBlock_scripts = function (hook_name, args, cb) {
-    args.content = args.content + '<script type="text/javascript">var nav_script = document.createElement("script");nav_script.type = "text/javascript";nav_script.src="https://nav.framapad.org/nav/nav.js";document.getElementsByTagName("head")[0].appendChild(nav_script);</script>';
+    args.content = args.content + '<script type="text/javascript">var nav_script = document.createElement("script");nav_script.type = "text/javascript";nav_script.src="https://framasoft.org/nav/nav.js";document.getElementsByTagName("head")[0].appendChild(nav_script);</script>';
 }
